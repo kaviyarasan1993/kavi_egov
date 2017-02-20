a2f496a336c4b8516942617ef5bddb9537bfcd3a
Kaviyarasan P <kaviyarasan.p@riflexions.com>
2017-02-20 10:52:12 +0530

    helper classes names changed to builder

---
+++ egov-lams/src/main/java/org/egov/lams/builder/AgreementQueryBuilder.java
@@ -0,0 +1,128 @@
+package org.egov.lams.builder;
+
+import java.util.List;
+
+import org.egov.lams.model.SearchAgreementsModel;
+
+public class AgreementQueryBuilder {
+
+	@SuppressWarnings("unchecked")
+	public static String agreementQueryBuilder(SearchAgreementsModel agreementsModel,
+			@SuppressWarnings("rawtypes") List preparedStatementValues) {
+
+		StringBuilder selectQuery = new StringBuilder("select * from eglams_agreement agreement");
+
+		if (!(agreementsModel.getAgreementId() == null && agreementsModel.getAgreementNumber() == null
+				&& (agreementsModel.getFromDate() == null && agreementsModel.getToDate() == null)
+				&& agreementsModel.getStatus() == null && agreementsModel.getTenderNumber() == null
+				&& agreementsModel.getTinNumber() == null && agreementsModel.getTradelicenseNumber() == null)) {
+			selectQuery.append(" where");
+			boolean isAppendAndClause = false;
+
+			if (agreementsModel.getAgreementId() != null) {
+				selectQuery.append(" agreement.id=" + getIdQuery(agreementsModel.getAgreementId()));
+				isAppendAndClause = addAndClauseIfRequired(isAppendAndClause, selectQuery);
+			}
+
+			if (agreementsModel.getAgreementNumber() != null) {
+				isAppendAndClause = addAndClauseIfRequired(isAppendAndClause, selectQuery);
+				selectQuery.append(" agreement.agreement_number=?");
+				preparedStatementValues.add(agreementsModel.getAgreementNumber());
+			}
+
+			if (agreementsModel.getStatus() != null) {
+				isAppendAndClause = addAndClauseIfRequired(isAppendAndClause, selectQuery);
+				selectQuery.append(" agreement.status=?");
+				preparedStatementValues.add(agreementsModel.getStatus().toString());
+			}
+
+			if (agreementsModel.getTenantId() != null) {
+				isAppendAndClause = addAndClauseIfRequired(isAppendAndClause, selectQuery);
+				selectQuery.append(" agreement.tenant_id=?");
+				preparedStatementValues.add(agreementsModel.getTenantId());
+			}
+
+			if (agreementsModel.getTenderNumber() != null) {
+				isAppendAndClause = addAndClauseIfRequired(isAppendAndClause, selectQuery);
+				selectQuery.append(" agreement.tender_number=?");
+				preparedStatementValues.add(agreementsModel.getTenderNumber());
+			}
+
+			if (agreementsModel.getTinNumber() != null) {
+				isAppendAndClause = addAndClauseIfRequired(isAppendAndClause, selectQuery);
+				selectQuery.append(" agreement.tin_number=?");
+				preparedStatementValues.add(agreementsModel.getTinNumber());
+			}
+
+			if (agreementsModel.getTradelicenseNumber() != null) {
+				isAppendAndClause = addAndClauseIfRequired(isAppendAndClause, selectQuery);
+				selectQuery.append(" agreement.TradeLicense_number=?");
+				preparedStatementValues.add(agreementsModel.getTradelicenseNumber());
+			}
+
+			if (agreementsModel.getAsset() != null) {
+				isAppendAndClause = addAndClauseIfRequired(isAppendAndClause, selectQuery);
+				selectQuery.append(" agreement.asset=?");
+				preparedStatementValues.add(agreementsModel.getAsset());
+			}
+
+			if (agreementsModel.getAllottee() != null) {
+				isAppendAndClause = addAndClauseIfRequired(isAppendAndClause, selectQuery);
+				selectQuery.append(" agreement.allottee=?");
+				preparedStatementValues.add(agreementsModel.getAllottee());
+			}
+
+			if (agreementsModel.getFromDate() != null) {
+
+				if (agreementsModel.getToDate() != null) {
+					if (agreementsModel.getToDate().compareTo(agreementsModel.getFromDate()) < 0)
+						throw new RuntimeException("ToDate cannot be lesser than fromdate");
+					isAppendAndClause = addAndClauseIfRequired(isAppendAndClause, selectQuery);
+					selectQuery.append(" agreement.agreement_date>=?");
+					preparedStatementValues.add(agreementsModel.getFromDate());
+					addAndClauseIfRequired(isAppendAndClause, selectQuery);
+					selectQuery.append(" agreement.agreement_date<=?");
+					preparedStatementValues.add(agreementsModel.getToDate());
+				}
+			}
+
+		}
+
+		/*
+		 * need to put default date value of one year span, considering current
+		 * date as toDate
+		 */
+
+		selectQuery.append(" ORDER BY agreement.id");
+		selectQuery.append(" LIMIT ?");
+		if (agreementsModel.getSize() != null)
+			preparedStatementValues.add(Integer.parseInt(agreementsModel.getSize()));
+		else
+			preparedStatementValues.add(20);
+
+		selectQuery.append(" OFFSET ?");
+
+		if (agreementsModel.getOffSet() != null)
+			preparedStatementValues.add(Integer.parseInt(agreementsModel.getOffSet()));
+		else
+			preparedStatementValues.add(0);
+
+		return selectQuery.toString();
+	}
+
+	private static boolean addAndClauseIfRequired(boolean appendAndClauseFlag, StringBuilder queryString) {
+
+		if (appendAndClauseFlag) {
+			queryString.append(" and");
+		}
+		return true;
+	}
+
+	private static String getIdQuery(List<Long> idList) {
+		StringBuilder query = new StringBuilder("" + idList.get(0));
+		for (int i = 1; i < idList.size(); i++) {
+			query.append("," + idList.get(i));
+		}
+		return query.toString();
+	}
+}
---
+++ egov-lams/src/main/java/org/egov/lams/model/wrapper/AgreementRequest.java
@@ -0,0 +1,23 @@
+package org.egov.lams.model.wrapper;
+
+import org.egov.lams.model.Agreement;
+import org.egov.lams.model.RequestInfo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class AgreementRequest   {
+  
+  private RequestInfo requestInfo = null;
+
+  private Agreement agreement = null;
+
+}
+
