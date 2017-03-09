06cd9527314a960e91119134ce47cc3f85925f62
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-03-09 11:56:37 +0530

    testing

--- egov-lams-persist/src/main/java/org/egov/lams/dao/AgreementDao.java
+++ egov-lams-persist/src/main/java/org/egov/lams/dao/AgreementDao.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-
+/***********************************************************************************************************************************/
 @Repository
 public class AgreementDao {
 
